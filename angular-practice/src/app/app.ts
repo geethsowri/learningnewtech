648035c94f825ecceabc05d653bc329aae875f68
geethsowri <nainalageethsowri@gmail.com>
2025-08-22 10:07:51 +0530

    learned how to create components with/without css/scss

--- angular-practice/src/app/app.ts
+++ angular-practice/src/app/app.ts
@@ -4,8 +4,16 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `
+    <h1>Welcome to {{ title() }}!</h1>
+    <p>hello! hey there</p>
+    <router-outlet />
+  `,
+  styles: [`
+    p {
+      color: blue;
+    }
+    `],
 })
 export class App {
   protected readonly title = signal('angular-practice');
