fe65fba4f580afafb13578247adf589116bd4cac
geethsowri <nainalageethsowri@gmail.com>
2025-08-23 10:36:18 +0530

    routes are done setting up

--- plotless/src/app/app.routes.ts
+++ plotless/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Home } from './home/home';
+import { Movie } from './movie/movie';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Login, pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'home', component:  Home },
+    { path: 'movie', component: Movie },
+];
